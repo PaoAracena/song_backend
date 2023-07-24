2d3fa28bc09b6c878d963b9aa055ad79c0b0f0f9
paoAracena <Paolaaracena1233@gmail.com>
2023-07-24 11:24:37 -0400

    have a small bug with server

---
+++ controllers/songController.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const songs = express.Router();
+const {
+  getAllSongs,
+  getSong,
+  createSong,
+  deleteSong,
+  updateSong,
+} = require("../queries/songs");
+const { checkName, checkBoolean } = require("../validations/checkSongs");
+
+
+//INDEX
+songs.get("/", async (req, res) => {
+    const allsongs = await getAllSongs();
+    if (allsongs[0]) {
+      res.status(200).json(allsongs);
+    } else {
+      res.status(500).json({ error: "server error" });
+    }
+  });
+
+  // SHOW
+songs.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    const song = await getSong(id);
+    if (song) {
+      res.json(song);
+    } else {
+      res.status(404).json({ error: "not found" });
+    }
+  });
+// CREATE
+songs.post("/",checkName, checkBoolean, async (req, res) => {
+    try {
+      const song = await createSong(req.body);
+      res.json(song);
+    } catch (error) {
+      res.status(400).json({ error: error });
+    }
+  });
+
+ //DELETE 
+  songs.delete("/:id", async (req, res) => {
+    const { id } = req.params;
+    const deletedSong = await deleteSong(id);
+    if (deletedSong.id) {
+      res.status(200).json(deletedSong);
+    } else {
+      res.status(404).json("Songs not found");
+    }
+  });
+
+// UPDATE
+songs.put("/:id",checkName, checkBoolean, async (req, res) => {
+  const { id } = req.params;
+  const updatedSong = await updateSong(id, req.body);
+  res.status(200).json(updatedSong);
+});
+
+module.exports = songs;
---
+++ db/seed.sql
@@ -0,0 +1,10 @@
+\c songs_dev;
+
+INSERT INTO songs (name, artist, album,time, is_favorite) VALUES
+
+('Latin','Bad Bunny','Un Verano Sin Ti', '1 hour 22 Mintues', true),
+('Urbano Latin','Karol G','Manana Sera Bonito', '53 Mintues', true),
+('Pop','Adele','30', '58 Mintues', false),
+('Pop','Olivia Rodrigo','Sour', '49 Mintues', false),
+('R&B/Soul','SZA','SoS', '1 Hour  8 Mintues', false),
+('Dance','Drake','Honestly Nevermind', '53 Mintues', true);
